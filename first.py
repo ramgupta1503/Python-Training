b7953ae0be2abfe9b982d6082769649465d53f20
ramgupta1503 <ramgupta15051@gmail.com>
2026-05-20 22:15:48 +0530

    First Upload

---
+++ first.py
@@ -0,0 +1,18 @@
+# print("Hello World")
+
+
+
+# print("My name is Ram Gupta")
+# print("My College name is Galgotia college of engineering and technology")
+
+
+
+# if 5>2:
+#     print("Five is greater than 2")
+
+
+
+name = "Ram Gupta"
+fav_subject = "Maths"
+print(name)
+print(fav_subject)
