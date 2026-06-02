5598f2bc8083dd1b818dcc0f4eb888a622b30b70
ramgupta1503 <ramgupta15051@gmail.com>
2026-06-02 23:10:59 +0530

    question practice

---
+++ questions.py
@@ -0,0 +1,25 @@
+# QUESTION --1
+
+# favouritecolors = {"red" , "black" , "green" , "blue"}
+# print(favouritecolors)
+
+# favouritecolors.add("red")
+# print(favouritecolors)
+
+# QUESTION --2
+
+# fruits = ["banana" , "mango" , "Orange" , "litchi"]
+# print(fruits[0])
+# print(fruits[3])
+
+# QUESTION --3
+
+# n1 = int(input("Enter first number : "))
+# n3 = int(input("Enter second number : "))
+# n2 = int(input("Enter third number : "))
+# n4 = int(input("Enter fourth number : "))
+# n5 = int(input("Enter fifth number : "))
+
+# numbers = [n1 , n2 , n3 , n4 , n5]
+# unique_numbers = set(numbers)
+# print(unique_numbers)
