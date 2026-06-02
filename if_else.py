8e8ef5f62677df664e7891c3eb41ca7d12085249
ramgupta1503 <ramgupta15051@gmail.com>
2026-06-02 23:34:39 +0530

    if-else statemnts

--- if_else.py
+++ if_else.py
@@ -1,5 +1,29 @@
-age = int(input("Enter age :"))
-if age >= 18:
-    print("Eligible for voting")
-else :
-    print("Not Eligible")
+# if-else condition
+
+# if condition :
+#     statement
+# else :
+#     statement
+
+
+
+
+# age = int(input("Enter age :"))
+# if age >= 18:
+#     print("Eligible for voting")
+# else :
+#     print("Not Eligible")
+
+
+
+
+# # if-elif-else
+
+#if condition1 :
+#     statement
+# elif condition2 :
+#     statement
+# elif condition3 :
+#     statement
+# else :
+#     statement
