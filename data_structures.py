4d33d6834b7a9746f26e08d0245ead2febb0c1fa
ramgupta1503 <ramgupta15051@gmail.com>
2026-06-02 23:11:54 +0530

    data structures

--- data_structures.py
+++ data_structures.py
@@ -37,7 +37,7 @@
 # DICTIONARY ---->
 
 # student = {
-#     "Name" : "Ram",
+#     "Name" : "Ram",   (key-value)
 #     "Age" : 20,
 #     "Course" : "CSE",
 # }
@@ -48,33 +48,3 @@
 # student["Grade"] = "A"
 # print(student)
 
-
-
-
-
-
-# QUESTION --1
-
-# favouritecolors = {"red" , "black" , "green" , "blue"}
-# print(favouritecolors)
-
-# favouritecolors.add("red")
-# print(favouritecolors)
-
-# QUESTION --2
-
-# fruits = ["banana" , "mango" , "Orange" , "litchi"]
-# print(fruits[0])
-# print(fruits[3])
-
-# QUESTION --3
-
-# n1 = int(input("Enter first number : "))
-# n3 = int(input("Enter second number : "))
-# n2 = int(input("Enter third number : "))
-# n4 = int(input("Enter fourth number : "))
-# n5 = int(input("Enter fifth number : "))
-
-# numbers = [n1 , n2 , n3 , n4 , n5]
-# unique_numbers = set(numbers)
-# print(unique_numbers)
