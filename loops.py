4f509c30828b5905467f7305e68a0e360b9692a1
ramgupta1503 <ramgupta15051@gmail.com>
2026-06-02 23:42:53 +0530

    loops

---
+++ loops.py
@@ -0,0 +1,10 @@
+# for variable in sequence :
+#     statement
+
+# for i in range(1,7):
+#     print(i)
+
+students = ["Ram" , "Arnav" , "Shubh" , "Daksh"]
+
+for i in students:
+    print("Hello" , i)
