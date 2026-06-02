6c49f698736e7fc61510f34ec0446bcba31c59a6
ramgupta1503 <ramgupta15051@gmail.com>
2026-06-02 22:19:08 +0530

    data strucyures

---
+++ data_structures.py
@@ -0,0 +1,14 @@
+# list = Shopping list (collection of same type of items)
+# tuple = Fixed Menu
+# Set = Bag of unique items
+# Dictionary = Dictionary with word meaning
+
+
+fruits = ["apple" , "mango" , "banana" , "cherry"]
+print(fruits)
+
+fruits.append("litchi") # .append is used to add new data type in sequence
+print(fruits)
+
+fruits.remove("apple") # .remove is used to remove any data type from the list
+print(fruits)
