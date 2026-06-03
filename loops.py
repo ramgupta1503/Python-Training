461ce993f6c9638e84c235a319e355bda73eaef1
ramgupta1503 <ramgupta15051@gmail.com>
2026-06-03 21:44:47 +0530

    loops

--- loops.py
+++ loops.py
@@ -1,10 +1,74 @@
 # for variable in sequence :
 #     statement
 
+
+
+
+
 # for i in range(1,7):
 #     print(i)
 
-students = ["Ram" , "Arnav" , "Shubh" , "Daksh"]
 
-for i in students:
-    print("Hello" , i)
+
+
+
+# students = ["Ram" , "Arnav" , "Shubh" , "Daksh"]
+
+# for i in students:
+#     print("Hello" , i)
+
+
+
+
+# for i in range(1,26):
+#     print(2*i , end = " ")
+
+
+
+
+
+# n = int(input("Enter the number : "))
+# sum = 0
+# for i in range(0,n+1):
+#     sum = sum + i
+# print(sum)
+
+
+
+
+# rows = 5
+ 
+# for i in range (1,rows+1):
+#     print("*" * i)
+
+
+
+
+# rows = 5
+# for i in range(1,rows+1):
+#     spaces = rows - i
+#     stars = 2 * i - 1
+#     print(" " * spaces + "*" * stars )
+
+
+
+
+# rows = 5
+# for i in range(1,rows):
+#     print("*" * (rows-i))
+
+
+
+
+# rows = 5
+# for i in range(1,rows):
+#     for j in range(1 , i):
+#         print(j , end = " ")
+#     print(i)
+
+
+
+
+word = "HELLO"
+for letter in word[:: -1]:
+    print(letter , end = " ")
