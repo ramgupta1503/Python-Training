c776b31f838286ed5148bc29d2a6d411394f5dca
ramgupta1503 <ramgupta15051@gmail.com>
2026-06-05 23:27:37 +0530

    Functions

---
+++ functions.py
@@ -0,0 +1,58 @@
+# def greet() :
+#     print("hello world")
+
+# greet()
+
+
+
+
+# def add(a , b) :
+#     return a + b
+
+# x = int(input("Enter first number : "))
+# y = int(input("Enter second number : "))
+# result = add(x,y)
+# print("Sum is " , result)
+
+
+
+# PRIME NUMBER 
+
+# x = int(input("Enter the number : "))
+# y = "no prime"
+# z = "prime"
+
+# def prime(x) :
+#     if x < 2 :
+#         return y
+#     for i in range(2,x):
+#         if x % i == 0 :
+#             return y
+#     else :
+#         return z
+
+# result = prime(x)
+# print(result)
+
+
+
+
+# QUESTION 
+
+# def aboutme() :
+#     print("RAM GUPTA")
+#     print("My age is 20")
+#     print("Currently living in Greater Noida")
+
+# aboutme()
+
+
+
+# SQUARE OF A NUMBER
+
+x = int(input("Enter the number : "))
+def square(num) :
+    return num*num
+
+result = square(x)
+print(result)
