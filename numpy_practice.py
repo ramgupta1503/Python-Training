ab565e246b7d0e475b7634b9e28561bd626557cb
ramgupta1503 <ramgupta15051@gmail.com>
2026-06-06 15:54:55 +0530

    numpy

---
+++ numpy_practice.py
@@ -0,0 +1,26 @@
+# import numpy as np
+
+# normal_list = [1 , 2 , 3 , 4 , 5]
+# normal_list
+
+# normal_list * 2
+
+
+# import numpy as np 
+
+# np_array = np.array([1 , 2 , 3 , 4 , 5])
+# np_array * 2
+# print(np_array * 2)
+
+
+# import numpy as np 
+
+# np_array = np.array([1 , 2 , 3 , 4 , 5 , 6])
+# print(np_array.reshape(3,2))
+
+
+# print((np_array * 5 ) - 3)
+
+
+
+
