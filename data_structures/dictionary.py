6171a6640d4c47d73ed2f31ad3ef76a645f0c8c7
ramgupta1503 <ramgupta15051@gmail.com>
2026-06-15 16:10:30 +0530

    dictionary

---
+++ data_structures/dictionary.py
@@ -0,0 +1,31 @@
+# DICTIONARY ---->
+
+# student = {
+#     "Name" : "Ram",   (key-value)
+#     "Age" : 20,
+#     "Course" : "CSE",
+# }
+ 
+# print(student)
+# print(student["Name"])
+
+# student["Grade"] = "A"
+# print(student)
+
+marks = {
+    "Ram" : 100,
+    "Arnav" : 99,
+    "Shubh" : 98
+}
+
+# print(marks, type(marks))
+# print(marks["Ram"])
+
+# print(marks.items())
+
+# print(marks.keys())
+# print(marks.values())
+# marks.update({"Ram": 99 , "Rishabh": 97})
+# print(marks)
+print(marks.get("Ram2"))
+print(marks["Ram2"])
---
+++ data_structures/sets.py
@@ -0,0 +1,10 @@
+# SETS ----> (Always stored in a curved brackets, Alwaya contains unique items)
+
+# students = {"Ram " , "Rudra" , "Arnav" , "Yuvraj"}
+# print(students)
+
+# students.add("Shubh")
+# print(students)
+
+# in sets we use .add instead of .append because
+# in sets order is not defined and .append add items at the end
