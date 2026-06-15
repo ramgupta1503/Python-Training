90cf9780d90f4c4037d754f619f3c99081e1f110
ramgupta1503 <ramgupta15051@gmail.com>
2026-06-15 14:28:03 +0530

    data_structures

---
+++ data_structures/lists.py
@@ -0,0 +1,50 @@
+# list = Shopping list (collection of same type of items)
+# tuple = Fixed Menu
+# Set = Bag of unique items
+# Dictionary = Dictionary with word meaning
+
+
+# LISTS ----> (Always stored in a large bracket)
+
+# fruits = ["apple" , "mango" , "banana" , "cherry"]
+# print(fruits)
+
+# fruits.append("litchi") # .append is used to add new data type in sequence
+# print(fruits)
+
+# fruits.remove("apple") # .remove is used to remove any data type from the list
+# print(fruits)
+
+
+# TUPLE ----> (Always stored in a small bracket)
+
+# coordinates = (99.25 , 45.47)
+# print(coordinates)
+
+
+# SETS ----> (Always stored in a curved brackets, Alwaya contains unique items)
+
+# students = {"Ram " , "Rudra" , "Arnav" , "Yuvraj"}
+# print(students)
+
+# students.add("Shubh")
+# print(students)
+
+# in sets we use .add instead of .append because
+# in sets order is not defined and .append add items at the end
+
+
+# DICTIONARY ---->
+
+# student = {
+#     "Name" : "Ram",   (key-value)
+#     "Age" : 20,
+#     "Course" : "CSE",
+# }
+ 
+# print(student)
+# print(student["Name"])
+
+# student["Grade"] = "A"
+# print(student)
+
