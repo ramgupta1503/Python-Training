6b7ea67421ac4f91456e43881e3754c46e1aa370
ramgupta1503 <ramgupta15051@gmail.com>
2026-06-15 15:44:22 +0530

    tuple

---
+++ data_structures/tuple.py
@@ -0,0 +1,11 @@
+# TUPLE ----> (Always stored in a small bracket)
+
+coordinates = (99.25 , 45.47)
+# print(coordinates)
+# print(type(coordinates))
+
+no = coordinates.count(45.47)
+print(no)
+
+i = coordinates.index(45.47)
+print(i)
