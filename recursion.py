0327ab74917d80b9e57534d1b58f05fd1acb77b3
ramgupta1503 <ramgupta15051@gmail.com>
2026-06-19 22:04:30 +0530

    Recursion

---
+++ recursion.py
@@ -0,0 +1,8 @@
+def factorial(n):
+    if n == 0 or n == 1: # base condition which doesn'r call the fucntion any further 
+        return 1
+    else:
+        return n*factorial(n-1) # fucntion calling itself
+
+a = factorial(3)
+print(a)
