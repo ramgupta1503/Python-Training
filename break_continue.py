735f1609dd667cf573770b10e8e139e95a2ec4c2
ramgupta1503 <ramgupta15051@gmail.com>
2026-06-19 14:37:51 +0530

    break and continue statement

---
+++ break_continue.py
@@ -0,0 +1,33 @@
+# BREAK STATEMENT
+
+# for i in range(100):
+    
+#     if(i==34):
+#         break
+    
+#     print(i)
+
+
+# CONTINUE STATEMENT
+
+# for i in range(100):
+    
+#     if(i==34):
+#         continue
+    
+#     print(i)
+
+
+
+for i in range(845):
+    pass
+
+i = 0 
+while (i<45):
+    print(i)
+    i = i+1
+
+ 
+
+for i in range (0,465):
+    print()
