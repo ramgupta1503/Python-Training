3b257e6b581a8d9ff560cb4ed5797e82bffe99de
ramgupta1503 <ramgupta15051@gmail.com>
2026-06-24 22:31:05 +0530

    CLASS

---
+++ OBJECT-ORIENTED-PROGRAMMING/class.py
@@ -0,0 +1,19 @@
+class Employee:
+    # name = "Ram"
+    language = "python"         # Class Attributes
+    salary = 120990
+
+
+# salary and language are class attributes as they directly belongs to the class
+
+
+ram = Employee()
+print(ram.language , ram.salary)
+
+arnav = Employee()
+arnav.name = "Arnav"    # Object Attributes / Istance Attributes
+print(arnav.name, arnav.language , arnav.salary)
+
+
+# Here name is object attributes / instance attributes
+
