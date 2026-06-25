648d28b6c0cfcbc235751c0a996ff5a8964f9e1c
ramgupta1503 <ramgupta15051@gmail.com>
2026-06-25 23:43:51 +0530

    functions and their detailS

---
+++ DATATYPES/functions/function.py
@@ -0,0 +1,23 @@
+# Functions:- are the predefined set of keyword in the languange. 
+# Functions:- help in let us do something in our program.
+# Functions:-  are very basic actions and verbs that the computer already know what to do for these functions.
+# eg. print(), input(), str, etc
+
+# Arguments:- is the input to a function that somehow influences its behaviour.
+# Side effect of the function can be audio video or anythin which is visible or not visible.
+
+
+# BUG:- bug is a mistake in a program.
+
+# Command-Line Interface (CLI):- is a text-based user interface used to interact with a computer's operating system, 
+# software, or files.
+
+# A user interface (UI):- is the space and set of elements through which a person interacts with a device,
+# application, or system.
+
+# In User interface, a programmer directly connects whatever is visible on the screen .
+
+# A Graphical User Interface (GUI):- is a visual-based interface that allows you to interact with a computer, smartphone,
+#  or electronic device using electronic producer elements like icons, menus, windows, and pointers.
+
+# Return Values :- 
