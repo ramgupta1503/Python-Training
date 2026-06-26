83b5f587ddd1266519db07fdfd6e58eb5ca9eca4
ramgupta1503 <ramgupta15051@gmail.com>
2026-06-26 23:49:26 +0530

    Different types of operators

---
+++ CALCULATOR/operator.py
@@ -0,0 +1,9 @@
+# Assignment operator:
+
+name = input("Enter you name: ")
+
+# operators that assign the value from right side to the left side 
+# example: = , 
+
+# right side value returns name which is assigned in the left side variable.
+# everytime the variable is called it will return the assigned value.
