fe607418f20b13d246063731f25c83315905e2f1
ramgupta1503 <ramgupta15051@gmail.com>
2026-06-26 23:48:08 +0530

    use of additional operator with different perspective

---
+++ CALCULATOR/addition.py
@@ -0,0 +1,14 @@
+# x = int(input("Enter the value of x: "))
+# y = int(input("Enter the value of y: "))
+
+# z = (x+y)
+
+# print(z)
+
+
+x = float(input("Enter the value of x: "))
+y = float(input("Enter the value of y: "))
+
+z = round(x+y)
+
+print(f"{z:, }")
