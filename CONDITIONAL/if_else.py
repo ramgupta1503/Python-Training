37f899f7a8b51b65e1a0a5a5edaf769d0b9f7804
ramgupta1503 <ramgupta15051@gmail.com>
2026-06-28 23:51:52 +0530

    if else with 'or' and 'and' functions

--- CONDITIONAL/if_else.py
+++ CONDITIONAL/if_else.py
@@ -29,10 +29,25 @@
 #     statement
 
 
-post = input("Enter the post: ")
+# post = input("Enter the post: ")
 
-if ("Ram".lower() in post.lower()):
-    print("This post is talking about Ram")
+# if ("Ram".lower() in post.lower()):
+#     print("This post is talking about Ram")
 
-else :
-    print("This post is not talking about Ram")
+# else :
+#     print("This post is not talking about Ram")
+
+
+
+
+
+# Use of "or" keyword
+
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+if x < y or x > y :
+    print("x is not equals to y")
+
+else:
+    print("x is equals to y")
