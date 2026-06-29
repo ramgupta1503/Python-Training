b6c5ca1eb353455ac82bba3792ea1473934e2c30
ramgupta1503 <ramgupta15051@gmail.com>
2026-06-29 23:01:41 +0530

    Uses of match and case statements

---
+++ CONDITIONAL/match.py
@@ -0,0 +1,36 @@
+# # match
+
+# name = input("What's your name?")
+
+# match name:
+#     case "Harry":
+#         print("Gryffindor")
+
+#     case "Ron":
+#         print("Gryffindor")
+
+#     case "Hermione":
+#         print("Gryffindor")
+
+#     case "Malfoy":
+#         print("Slytherin")
+
+#     case _:
+#         print("Who? ")
+
+
+
+#     # ANOTHER METHOD
+
+
+# match name:
+#     case "Harry":
+#         print("Gryffindor")
+        
+#     case "Malfoy":
+#         print("Slytherin")
+
+#     case _:
+#         print("Who? ")
+    
+
