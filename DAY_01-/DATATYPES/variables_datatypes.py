839fcc8f74a0cf063090fcd379bb1472388da0b6
ramgupta1503 <ramgupta15051@gmail.com>
2026-06-30 14:25:49 +0530

    More variables like int and float

---
+++ DAY_01-/DATATYPES/variables_datatypes.py
@@ -0,0 +1,40 @@
+# variables are created on the computer memory and that variable can store value.
+# Variable is just a container for the value.
+
+
+# age = 16     #int
+# number = 829.1312     #float
+# name = "Ram"     #text
+# did he pass the exam = "true/false"     #boolean
+
+
+# a = 5
+# b = 6
+# print(a+b)
+
+
+# a = "Hello"
+# b = "World"
+# print(a + " " + b)
+
+
+# For knowing which data type we used
+# print(type("Hello"))
+
+
+# a = 4
+# b = 2
+# print("Addition : " , a + b)
+# print("Subtraction : " , a - b)
+# print("Multiplication : " , a * b)
+# print("Division : " , a / b)
+
+
+
+# age = 20
+# print("I am" + " " + str(age) + " year old")
+
+
+a = int(input("First Number"))
+b = int(input("Second Number"))
+print(a + b)
