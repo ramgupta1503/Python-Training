728a8722ebf3315e3afd15a52f75b8f11a9cc9d4
ramgupta1503 <ramgupta15051@gmail.com>
2026-06-30 14:28:28 +0530

    Leap year code using functions

---
+++ DAY_01-/FUNCTIONS/leap_year.py
@@ -0,0 +1,17 @@
+# def leap_year(year):
+#     leap = False 
+
+#     if ((year % 400 != 0) and (year % 100 == 0)):
+#         return leap
+
+#     elif (year % 4 != 0):
+#         return leap 
+    
+#     else:
+#         return True
+
+# year = int(input("Enter the year: "))
+# print(leap_year(year))
+
+
+
