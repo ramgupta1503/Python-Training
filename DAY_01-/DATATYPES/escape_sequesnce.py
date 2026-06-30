19ddd7d636ea3ccc26fdb5c272c1da3b3b471e39
ramgupta1503 <ramgupta15051@gmail.com>
2026-06-30 14:25:09 +0530

    Escape sequence of \n, \t and etc.

---
+++ DAY_01-/DATATYPES/escape_sequesnce.py
@@ -0,0 +1,14 @@
+# \n new line character
+
+# a = "Ram is a good boy \nbut not a bad boy"
+# print(a)
+
+
+# \t tab character providing space
+
+a = "Ram is a good boy \tbut not a bad boy"
+print(a)
+
+
+# \; for single quote
+# \ and more characters
