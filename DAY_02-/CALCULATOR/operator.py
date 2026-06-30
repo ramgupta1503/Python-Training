a558c6ff8169f4f0d959ed29166f0c076e16beec
ramgupta1503 <ramgupta15051@gmail.com>
2026-06-30 14:31:34 +0530

    Operator and its theory

---
+++ DAY_02-/CALCULATOR/operator.py
@@ -0,0 +1,15 @@
+# Assignment operator:
+
+name = input("Enter you name: ")
+
+# operators that assign the value from right side to the left side 
+# example: = , 
+
+# right side value returns name which is assigned in the left side variable.
+# everytime the variable is called it will return the assigned value.
+
+# OPERATORS
+
+# + , - , * , / , %
+
+
