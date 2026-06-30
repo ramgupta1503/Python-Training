8a6a4cec8a999af022193ecc04dfcbaa13aa10ae
ramgupta1503 <ramgupta15051@gmail.com>
2026-06-30 14:35:02 +0530

    If else statements use

---
+++ DAY_02-/CONDITIONAL/if_else.py
@@ -0,0 +1,53 @@
+# if-else condition
+
+# if condition :
+#     statement
+# else :
+#     statement
+
+
+
+
+# age = int(input("Enter age :"))
+# if age >= 18:
+#     print("Eligible for voting")
+# else :
+#     print("Not Eligible")
+
+
+
+
+# # if-elif-else
+
+#if condition1 :
+#     statement
+# elif condition2 :
+#     statement
+# elif condition3 :
+#     statement
+# else :
+#     statement
+
+
+# post = input("Enter the post: ")
+
+# if ("Ram".lower() in post.lower()):
+#     print("This post is talking about Ram")
+
+# else :
+#     print("This post is not talking about Ram")
+
+
+
+
+
+# Use of "or" keyword
+
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+if x < y or x > y :
+    print("x is not equals to y")
+
+else:
+    print("x is equals to y")
