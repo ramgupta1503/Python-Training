8a5ce56403decf02d3d86447ebc9542e12fdf0f8
ramgupta1503 <ramgupta15051@gmail.com>
2026-07-01 00:07:51 +0530

    Lists and lists with loops

---
+++ DAY_03-/DATA-STRUCTURES/list.py
@@ -0,0 +1,38 @@
+student = ["Hermione", "Harry", "Ron", "Draco"]
+#              0           1       2      3   
+
+houses = ["Griffyndor", "Griffyndor", "Griffyndor", "Slytherin"]
+
+print(student[0])
+
+
+
+# LISTS ----> (Always stored in a large bracket)
+
+# fruits = ["apple" , "mango" , "banana" , "cherry"]
+# print(fruits)
+
+# fruits.append("litchi") # .append is used to add new data type in sequence
+# print(fruits)
+
+# fruits.remove("apple") # .remove is used to remove any data type from the list
+# print(fruits)
+
+# print(fruits[0])
+
+# fruits[0] = "Grapes"
+# print(fruits[0])
+
+# UNLIKE AS STRING, LISTS ARE MUTABLE
+# print(fruits[1:3])
+# print(fruits[0:4:2])
+
+l1 = [1, 1123, 313, 2, 114, 131]
+# l1.sort()
+# l1.reverse()
+
+# l1.insert(2 , 748)
+# l1.pop(2)
+# print(l1)
+l1.remove(313)
+print(l1)
