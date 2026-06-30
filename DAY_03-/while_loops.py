a7e23b88b7f6b87a93f8e6c35baaa5b38fa107e3
ramgupta1503 <ramgupta15051@gmail.com>
2026-06-30 14:22:07 +0530

    While loops and their uses

---
+++ DAY_03-/while_loops.py
@@ -0,0 +1,18 @@
+#  WHILE LOOP SYNTAX
+
+# while condition:
+#     code block
+    
+
+# count = 1
+    
+# while count <= 5 :
+#     print(count)
+#     count += 1 
+
+
+i = 1
+
+while (i<6) :
+    print(i)
+    i += 1
