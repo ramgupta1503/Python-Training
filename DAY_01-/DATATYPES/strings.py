6752f2135194660c1feef0424019584264f3cc6d
ramgupta1503 <ramgupta15051@gmail.com>
2026-06-30 14:24:33 +0530

    Strings and its commands

---
+++ DAY_01-/DATATYPES/strings.py
@@ -0,0 +1,48 @@
+# name = "price"
+
+# nameshort = name[0:5]
+
+# print(nameshort)
+
+# # character4 = name[4]
+# # print(character4)
+
+# print(name[-4:-1])
+# print(name[1:4])
+
+# print(name[:4]) # if first number is missing then it starts form 0 
+# print(name[1:]) # if second number is missing then it's last number is (total number - 1)
+
+# print(name[3:3])
+
+
+# LENGTH AND MORE FUNCTIONS
+
+# Mystring = "hello"
+# print(len(Mystring))
+# print(Mystring.endswith("llo"))
+# print(Mystring.endswith("LLO"))
+# print(Mystring.startswith("ha"))
+# print(Mystring.capatilize())
+
+
+# string = "abracadabra"
+# count = string.count("c")
+# print(count)
+
+
+# s = "HELLO WORLD"
+# index = s.find("WORLD")
+# print(index)
+
+
+# S = "Hello World"
+# replaced_string = S.replace("World" , "Python")
+# print(replaced_string)
+
+
+name = "Ram is a good   boy"
+
+print(name.find("  "))
+
+# STRINGS ARE IMMUTABBLE AND CANNOT BE CHANGED ONLY REPLACED TO NEW STRING
