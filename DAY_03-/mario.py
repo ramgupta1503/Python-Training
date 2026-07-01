7f76f61a8dc139289f8caa3455a273497dd73b15
ramgupta1503 <ramgupta15051@gmail.com>
2026-07-01 22:24:57 +0530

    Adding mario blocks

---
+++ DAY_03-/mario.py
@@ -0,0 +1,78 @@
+# for _ in range(3):
+#     print("#")
+
+
+
+
+#    OR 
+
+
+# def main():
+#     print_column(3)
+
+
+# def print_column(height):
+#     for _ in range(height):
+#         print("#")
+
+# main()
+
+
+#    AND 
+
+
+# def main():
+#     print_row(4)
+
+
+# def print_row(width):
+#     print("?" * width )
+
+
+# main()
+
+
+
+#    AND
+
+
+# def main():
+#     print_square(3)
+
+
+
+# def print_square(size):
+
+#     # For each row in square
+#     for i in range(size):
+
+#     # For each brick in row
+#         for j in range(size):
+
+#             # Print brick
+#             print("#" , end ="")
+
+#             # Shifting to new row after printing brick in one row
+#         print()
+
+
+
+# main()
+
+
+
+
+def main():
+    print_square(3)
+
+
+
+def print_square(size):
+
+    # For each row in square
+    for i in range(size):
+            print("#" * size)
+
+
+
+main()
