0f301022e4db613d2cb1270f570db5c919fffc15
ramgupta1503 <ramgupta15051@gmail.com>
2026-07-01 22:27:31 +0530

    For loops and uses of for loops and lists

---
+++ DAY_03-/LOOPS/for_loops.py
@@ -0,0 +1,103 @@
+# for variable in sequence :
+#     statement
+
+
+
+
+
+# for i in range(1,7):
+#     print(i)
+
+
+
+
+
+# students = ["Ram" , "Arnav" , "Shubh" , "Daksh"]
+
+# for i in students:
+#     print("Hello" , i)
+
+
+
+
+# for i in range(1,26):
+#     print(2*i , end = " ")
+
+
+
+
+
+# n = int(input("Enter the number : "))
+# sum = 0
+# for i in range(0,n+1):
+#     sum = sum + i
+# print(sum)
+
+
+
+
+# rows = 5
+ 
+# for i in range (1,rows+1):
+#     print("*" * i)
+
+
+
+
+# rows = 5
+# for i in range(1,rows+1):
+#     spaces = rows - i
+#     stars = 2 * i - 1
+#     print(" " * spaces + "*" * stars )
+
+
+
+
+# rows = 5
+# for i in range(1,rows):
+#     print("*" * (rows-i))
+
+
+
+
+# rows = 5
+# for i in range(1,rows):
+#     for j in range(1 , i):
+#         print(j , end = " ")
+#     print(i)
+
+
+
+
+# word = "HELLO"
+# for letter in word[:: -1]:
+#     print(letter , end = " ")
+
+
+
+# for i in range (start , stop , stepsize):
+    #code
+
+
+
+# l = [12,3,4,533,1231,342,121,]
+
+# for i in l:
+#     print(i)
+
+
+
+# n = int(input("Enter the number: "))
+
+# for i in range (1,n+1):
+#     if(i == 1 or i == n):
+#         print("*"*n)
+#     else:
+#         print("*" , end="")
+#         print(" "*(n-2) , end = "")
+#         print("*")
+
+
+
+
+print("meow\n" *3 , end = "")
